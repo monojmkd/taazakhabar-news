5e300b353a32366626d13cfc60eabb6b96b41c6d
Monoj Kumar Das <49304400+monojmkd@users.noreply.github.com>
2023-08-05 17:24:16 +0530

    api changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar.js';
 import News from './components/News';
-
+import image from './media/1.png'
 
 function App() {
   return (
     <>
+    <div style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+    <img src={image} style={{maxWidth:"50%"}} alt="..."></img>
+    </div>
    <NavBar/>
   <News/>
    </>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,27 +1,29 @@
-import React, { Component } from 'react'
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import { Container } from "react-bootstrap";
+import React, { Component } from "react";
 
 export default class NewsItem extends Component {
-       
   render() {
     let { title, description, imageUrl, newsURL, author, date, source } =
-    this.props;
-  let defUrlImg ="https://static1.anpoimages.com/wordpress/wp-content/uploads/2023/04/google-io-2023.jpeg";
+      this.props;
+    let defUrlImg =
+      "https://static1.anpoimages.com/wordpress/wp-content/uploads/2023/04/google-io-2023.jpeg";
     return (
-        <Container className="pt-4">
-        <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src={imageUrl} />
-              <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>
-                  {description}...
-                </Card.Text>
-                <Button variant="primary" href={newsURL} target="_blank" >Read More</Button>
-              </Card.Body>
-            </Card>
-            </Container>
+      <div className="card mb-3" style={{maxWidth: "100%"}}>
+       <div class="row g-0">
+    <div class="col-md-4">
+    <img src={imageUrl} class="card-img-top" alt="..." />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">{title}</h5>
+        <p class="card-text">{description}...</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        <a variant="primary" href={newsURL} target="_blank"  rel="noreferrer" >
+            Read More
+          </a>
+      </div>
+    </div>
+      </div>
+      </div>
     );
   }
 }
