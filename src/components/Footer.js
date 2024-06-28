095317ef63d47547797c0dedd1c432955e792ea8
Monoj Kumar Das <49304400+monojmkd@users.noreply.github.com>
2024-06-28 17:50:42 +0530

    responsive and CSS updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,17 @@
 import React from "react";
+import "../css/Footer.css";
 
 const Footer = () => {
   return (
     <div>
       <footer className="py-5 bg-dark">
         <div className="container px-4">
-          <p
-            className="m-0 text-center text-white"
-            style={{ fontWeight: "500" }}
-          >
+          <p className="m-0 text-center text-white">
             Copyright &copy; TaazaKhabr | By{" "}
             <a
               href="https://github.com/monojmkd"
               target="_blank"
               rel="noreferrer"
-              style={{ textDecoration: "none", color: "#FEE715ff" }}
             >
               Monoj Kumar Das
             </a>
--- src/components/News.js
+++ src/components/News.js
@@ -1,64 +1,53 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import NewsItem from "./NewsItem";
 import propTypes from "prop-types";
 import Spinner from "./Spinner";
+import "../css/News.css";
 
-const News = (props) => {
+const News = ({ category, setProgress }) => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
+
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
-  const updateNews = async () => {
-    props.setProgress(10);
-    // const url = `https://inshorts.me/news/topics/${props.category}`;
-    // const url = `https://taazakhabr-cors.vercel.app/proxy/inshorts/api/en/search/trending_topics/${props.category}?page=1&type=NEWS_CATEGORY`;
-    const url = `https://inshortsapi.vercel.app/news?category=${props.category}`;
+
+  const updateNews = useCallback(async () => {
+    setProgress(10);
+    const url = `https://inshortsapi.vercel.app/news?category=${category}`;
     setLoading(true);
     let data = await fetch(url);
-    props.setProgress(30);
+    setProgress(30);
     let parsedData = await data.json();
-    // console.log("data", parsedData.data);
-    props.setProgress(70);
+    setProgress(70);
     const newsData = parsedData.data;
-    console.log(newsData);
+    // console.log(newsData);
     setArticles(newsData);
     setLoading(false);
-    props.setProgress(100);
-  };
+    setProgress(100);
+  }, [category, setProgress]);
 
-  //The useEffect Hook allows you to perform side effects in your components.
   useEffect(() => {
-    document.title = `TaazaKhabr - ${capitalizeFirstLetter(props.category)} `;
-    updateNews(); // eslint-disable-next-line
-  }, []);
+    document.title = `TaazaKhabr - ${capitalizeFirstLetter(category)}`;
+    updateNews();
+  }, [category, updateNews]);
 
   return (
     <div className="container">
-      <h2
-        style={{
-          fontFamily: "'Dai Banna SIL', serif",
-          textDecoration: "underline",
-        }}
-      >
-        Fresh News at Your Fingertips : {capitalizeFirstLetter(props.category)}{" "}
-      </h2>
+      <h2>Fresh News at Your Fingertips : {capitalizeFirstLetter(category)}</h2>
       {loading && <Spinner />}
-      {articles.map((element) => {
-        return (
-          <div key={element.id}>
-            <NewsItem
-              title={element.title ? element.title.slice(0, 100) : ""}
-              description={element.content ? element.content.slice(0, 170) : ""}
-              imageUrl={element.imageUrl}
-              newsURL={element.readMoreUrl}
-              author={element.author}
-              date={element.date}
-              // source={element.url}
-            />
-          </div>
-        );
-      })}
+      {articles.map((element) => (
+        <div className="news" key={element.id}>
+          <NewsItem
+            title={element.title ? element.title.slice(0, 100) : ""}
+            description={element.content ? element.content.slice(0, 180) : ""}
+            imageUrl={element.imageUrl}
+            newsURL={element.readMoreUrl}
+            author={element.author}
+            date={element.date}
+          />
+        </div>
+      ))}
     </div>
   );
 };
@@ -69,6 +58,7 @@ News.defaultProps = {
 
 News.propTypes = {
   category: propTypes.string,
+  setProgress: propTypes.func.isRequired,
 };
 
 export default News;
